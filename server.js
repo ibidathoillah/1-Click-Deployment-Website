5b587f17fad737b8edb99277bc4f0a3f15e24a1d
Ibid Athoillah <ibidathoillah@gmail.com>
2021-05-14 17:53:10 +0700

    change acme express

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var ghost = require('ghost');
 var parentApp = require('express')()
 
-parentApp.get('/.well-known/acme-challenge/jgqtv7brrnlgympkbjkg794g5zbhpihjci6e7fpv2t4', function (req, res) {
-  res.send('jGqTv7BRrNlGYMPKBjKg794g5zBhPIHjcI6E7fpv2T4')
+parentApp.get('/.well-known/acme-challenge/OaIFhUES_l3HUPo-dAjY3rprqSdikXOMQ_FRwGLZwcs', function (req, res) {
+  res.send('OaIFhUES_l3HUPo-dAjY3rprqSdikXOMQ_FRwGLZwcs.7j72fN1OvFKTpobAWCJZkPWmt7YCczBTX368J3wy2uY')
 })
 
 // Run a single Ghost process
