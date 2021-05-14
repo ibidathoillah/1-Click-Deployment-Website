6508bc831ff07b762ec32a65a581e6e6755d29c6
Ibid Athoillah <ibidathoillah@gmail.com>
2021-05-14 17:49:43 +0700

    add acme to express routing

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var ghost = require('ghost');
+var parentApp = require('express')()
+
+parentApp.get('/.well-known/acme-challenge/jgqtv7brrnlgympkbjkg794g5zbhpihjci6e7fpv2t4', function (req, res) {
+  res.send('jGqTv7BRrNlGYMPKBjKg794g5zBhPIHjcI6E7fpv2T4')
+})
 
 // Run a single Ghost process
 ghost()
-  .then( ghostServer => ghostServer.start() )
+  .then( ghostServer => {
+    parentApp.use("/", ghostServer.rootApp);
+    ghostServer.start(parentApp)
+  })
   .catch( error => {
     console.error(`Ghost server error: ${error.message} ${error.stack}`);
     process.exit(1);
